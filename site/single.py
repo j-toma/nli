d53f45ef55a366838ad85f67b5a1d0b8c9141d34
jtoma <j.toma05@gmail.com>
2020-07-01 19:20:41 -0700

    comment out unnecessary in single

--- site/single.py
+++ site/single.py
@@ -18,63 +18,6 @@ pipeline = load(model_file)
 clf = pipeline.named_steps.svc
 vectorizer = pipeline.named_steps.tfidf
 
-def display(c,v):
-    '''
-        input content, vectorizer
-        output formatted content according to features of model
-    '''
-    feature_names = vectorizer.get_feature_names() 
-    #feature_names = p.named_steps.tfidf.get_feature_names() 
-    coefs_with_fns = sorted(zip(clf.coef_[0], feature_names)) 
-    
-    strong_non = coefs_with_fns[:100]
-    count_strong_non = 0
-    strong_non_list = []
-
-    weak_non = coefs_with_fns[100:2000]
-    count_weak_non = 0
-    weak_non_list = []
-
-    weak_nat = coefs_with_fns[-2000:-100]
-    count_weak_nat = 0
-    weak_nat_list = []
-
-    strong_nat = coefs_with_fns[-100:]
-    count_strong_nat = 0
-    strong_nat_list = []
-
-    #c = c.replace('\n', '')
-    #c = escape(c)
-    for phrase in strong_non:
-        spaced_phrase = ' ' + phrase[1] + ' '
-        if spaced_phrase in c:
-            count_strong_non += 1
-            replacement = '<span style="color:red">' + spaced_phrase + '</span>'
-            c = c.replace(spaced_phrase, replacement)
-            #matches = re.finditer(spaced_phrase, c)
-    for phrase in weak_non:
-        spaced_phrase = ' ' + phrase[1] + ' '
-        if spaced_phrase in c:
-            count_weak_non += 1
-            replacement = '<span style="color:orange">' + spaced_phrase + '</span>'
-            c = c.replace(spaced_phrase, replacement)
-    for phrase in weak_nat:
-        spaced_phrase = ' ' + phrase[1] + ' '
-        if spaced_phrase in c:
-            count_weak_nat += 1
-            replacement = '<span style="color:green">' + spaced_phrase + '</span>'
-            c = c.replace(spaced_phrase, replacement)
-    for phrase in strong_nat:
-        spaced_phrase = ' ' + phrase[1] + ' '
-        if spaced_phrase in c:
-            count_strong_nat += 1
-            replacement = '<span style="color:yellowgreen">' + spaced_phrase + '</span>'
-            c = c.replace(spaced_phrase, replacement)
-    print('num features strongly indicating nonnative:', count_strong_non)
-    print('num features weakly indicating nonnative:', count_weak_non)
-    print('num features weakly indicating native:', count_weak_nat)
-    print('num features strongly indicating native:', count_strong_nat)
-    return c
 
 def feature_in_sentence(l, s):
     '''
@@ -165,45 +108,103 @@ def display_text(content):
     content = display_list(content, vectorizer)
     return prediction, proba, content 
 
-def run_display(filename):
-    os.chdir('/home/jtoma/nli/data')
-    with open(filename) as f:
-        content =  f.read()
-        content_vectorized = vectorizer.transform([content])
-        prediction = clf.predict(content_vectorized)
-        print('prediction:', prediction)
-        proba = clf.decision_function(content_vectorized)
-        print('proba:', proba)
-        content = display(content, vectorizer)
-        content = Markup(content)
-        #formatted_content = display(content, vectorizer)
-        #print(formatted_content)
-        #print_formatted_text(HTML(formatted_content))
-        #display(Markdown((formatted_content)))
-        #mkdn = Markdown((formatted_content))
-        #unescaped = html_parser.unescape(formatted_content)
+#def display(c,v):
+#    '''
+#        input content, vectorizer
+#        output formatted content according to features of model
+#    '''
+#    feature_names = vectorizer.get_feature_names() 
+#    #feature_names = p.named_steps.tfidf.get_feature_names() 
+#    coefs_with_fns = sorted(zip(clf.coef_[0], feature_names)) 
+#    
+#    strong_non = coefs_with_fns[:100]
+#    count_strong_non = 0
+#    strong_non_list = []
+#
+#    weak_non = coefs_with_fns[100:2000]
+#    count_weak_non = 0
+#    weak_non_list = []
+#
+#    weak_nat = coefs_with_fns[-2000:-100]
+#    count_weak_nat = 0
+#    weak_nat_list = []
+#
+#    strong_nat = coefs_with_fns[-100:]
+#    count_strong_nat = 0
+#    strong_nat_list = []
+#
+#    #c = c.replace('\n', '')
+#    #c = escape(c)
+#    for phrase in strong_non:
+#        spaced_phrase = ' ' + phrase[1] + ' '
+#        if spaced_phrase in c:
+#            count_strong_non += 1
+#            replacement = '<span style="color:red">' + spaced_phrase + '</span>'
+#            c = c.replace(spaced_phrase, replacement)
+#            #matches = re.finditer(spaced_phrase, c)
+#    for phrase in weak_non:
+#        spaced_phrase = ' ' + phrase[1] + ' '
+#        if spaced_phrase in c:
+#            count_weak_non += 1
+#            replacement = '<span style="color:orange">' + spaced_phrase + '</span>'
+#            c = c.replace(spaced_phrase, replacement)
+#    for phrase in weak_nat:
+#        spaced_phrase = ' ' + phrase[1] + ' '
+#        if spaced_phrase in c:
+#            count_weak_nat += 1
+#            replacement = '<span style="color:green">' + spaced_phrase + '</span>'
+#            c = c.replace(spaced_phrase, replacement)
+#    for phrase in strong_nat:
+#        spaced_phrase = ' ' + phrase[1] + ' '
+#        if spaced_phrase in c:
+#            count_strong_nat += 1
+#            replacement = '<span style="color:yellowgreen">' + spaced_phrase + '</span>'
+#            c = c.replace(spaced_phrase, replacement)
+#    print('num features strongly indicating nonnative:', count_strong_non)
+#    print('num features weakly indicating nonnative:', count_weak_non)
+#    print('num features weakly indicating native:', count_weak_nat)
+#    print('num features strongly indicating native:', count_strong_nat)
+#    return c
 
-        return prediction, proba, content 
-
-
-def display_upload(content):
-    if type(content) == str:
-        pass
-    else:
-        content = content.read().decode('utf-8')
-    content_vectorized = vectorizer.transform([content])
-    prediction = clf.predict(content_vectorized)
-    print('prediction:', prediction)
-    proba = clf.decision_function(content_vectorized)
-    print('proba:', proba)
-    content = display(content, vectorizer)
-    content = Markup(content)
-    #formatted_content = display(content, vectorizer)
-    #print(formatted_content)
-    #print_formatted_text(HTML(formatted_content))
-    #display(Markdown((formatted_content)))
-    #mkdn = Markdown((formatted_content))
-    #unescaped = html_parser.unescape(formatted_content)
-
-    #return prediction, proba, content 
-    return prediction, proba, content 
+#def run_display(filename):
+#    os.chdir('/home/jtoma/nli/data')
+#    with open(filename) as f:
+#        content =  f.read()
+#        content_vectorized = vectorizer.transform([content])
+#        prediction = clf.predict(content_vectorized)
+#        print('prediction:', prediction)
+#        proba = clf.decision_function(content_vectorized)
+#        print('proba:', proba)
+#        content = display(content, vectorizer)
+#        content = Markup(content)
+#        #formatted_content = display(content, vectorizer)
+#        #print(formatted_content)
+#        #print_formatted_text(HTML(formatted_content))
+#        #display(Markdown((formatted_content)))
+#        #mkdn = Markdown((formatted_content))
+#        #unescaped = html_parser.unescape(formatted_content)
+#
+#        return prediction, proba, content 
+#
+#
+#def display_upload(content):
+#    if type(content) == str:
+#        pass
+#    else:
+#        content = content.read().decode('utf-8')
+#    content_vectorized = vectorizer.transform([content])
+#    prediction = clf.predict(content_vectorized)
+#    print('prediction:', prediction)
+#    proba = clf.decision_function(content_vectorized)
+#    print('proba:', proba)
+#    content = display(content, vectorizer)
+#    content = Markup(content)
+#    #formatted_content = display(content, vectorizer)
+#    #print(formatted_content)
+#    #print_formatted_text(HTML(formatted_content))
+#    #display(Markdown((formatted_content)))
+#    #mkdn = Markdown((formatted_content))
+#    #unescaped = html_parser.unescape(formatted_content)
+#
+#    #return prediction, proba, content 
+#    return prediction, proba, content 
